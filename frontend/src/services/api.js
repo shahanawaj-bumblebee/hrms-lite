081ba17668935f89c7c3b732ecb45614d5c296fc
shahanawaj-bumblebee <mdshahanawaj1219@gmail.com>
2026-02-12 16:42:01 +0530

    fix api syntax

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL:
-    import.meta.env.VITE_API_URL = https://hrms-lite-3-q4lp.onrender.com/api,
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 export default API;
